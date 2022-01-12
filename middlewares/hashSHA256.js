b2eaebedbe8cfd98766c4f3d17c950a3b3ddea75
dattte <72026353+dattte@users.noreply.github.com>
2022-01-12 10:53:50 +0700

    Update hashSHA256.js

--- middlewares/hashSHA256.js
+++ middlewares/hashSHA256.js
@@ -3,5 +3,5 @@ const crypto = require("crypto");
 module.exports = function (stringHash) {
   const hash = crypto.createHash("sha512");
   var data = hash.update(stringHash, "utf-8");
-  return data.digest("hex").toString();
+  return data.digest("hex");
 };
